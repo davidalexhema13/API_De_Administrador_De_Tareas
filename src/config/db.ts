2a83881452489854e70e131cd83634d2bb84c2a9
davidalexhema13 <alexander31667400@gmail.com>
2026-03-31 14:55:45 -0500

    corrección: usar ts-ignore para compatibilidad con la importación de PrismaClient

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,29 +1,17 @@
-/**
- * Configuración del Cliente de Prisma con Adaptador Nativo para Neon.
- * 
- * Se utiliza '@prisma/adapter-neon' para optimizar las consultas a través de 
- * WebSockets, permitiendo una conexión más estable y rápida en entornos en la nube.
- * 
- * @module PrismaConfig
- */
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
+// @ts-ignore
+import pkg from "@prisma/client";
 import * as dotenv from "dotenv";
 
-// Carga las variables de entorno (.env)
 dotenv.config();
 
-/**
- * Adaptador de conexión para PostgreSQL en Neon.
- * Utiliza la variable de entorno DATABASE_URL.
- */
+// @ts-ignore
+const { PrismaClient } = pkg;
+
 const adapter = new PrismaNeon({ 
     connectionString: process.env.DATABASE_URL as string 
 });
 
-/**
- * Instancia única (Singleton) del cliente de Prisma para toda la aplicación.
- */
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
